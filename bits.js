58027bd9b91dfc33dc271ce76d6fb7af900fcb0a
Bobby Roe <bobby.roe@unifyconsulting.com>
2025-04-20 17:50:31 -0700

    reorder bits.js

--- bits.js
+++ bits.js
@@ -1,26 +1,3 @@
-// 1) post-processing
-import { EffectComposer } from "jsm/postprocessing/EffectComposer.js";
-import { RenderPass } from "jsm/postprocessing/RenderPass.js";
-import { UnrealBloomPass } from "jsm/postprocessing/UnrealBloomPass.js";
-
-// post-processing
-const renderScene = new RenderPass(scene, camera);
-const bloomPass = new UnrealBloomPass(new THREE.Vector2(w, h), 1.5, 0.4, 100);
-bloomPass.threshold = 0.002;
-bloomPass.strength = 2.0;
-bloomPass.radius = 0;
-const composer = new EffectComposer(renderer);
-composer.addPass(renderScene);
-composer.addPass(bloomPass);
-
-// 2) Orbit controls
-import { OrbitControls } from 'jsm/controls/OrbitControls.js';
-//
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.03;
-// render
-controls.update();
 
 // 3) physics
 import { getBody, getMouseBall } from "./getBodies.js";
@@ -50,3 +27,29 @@ world.step();
   5) add Orbit Controls
   6) add post processing
 */
+
+
+
+// 1) post-processing
+import { EffectComposer } from "jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "jsm/postprocessing/UnrealBloomPass.js";
+
+// post-processing
+const renderScene = new RenderPass(scene, camera);
+const bloomPass = new UnrealBloomPass(new THREE.Vector2(w, h), 1.5, 0.4, 100);
+bloomPass.threshold = 0.002;
+bloomPass.strength = 2.0;
+bloomPass.radius = 0;
+const composer = new EffectComposer(renderer);
+composer.addPass(renderScene);
+composer.addPass(bloomPass);
+
+// 2) Orbit controls
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+//
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+// render
+controls.update();
