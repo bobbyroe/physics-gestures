32d91e502355c455fdd25f41d110d2e5321a5d2c
Bobby Roe <bobby.roe@unifyconsulting.com>
2025-05-11 20:37:28 -0700

    physics!

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import * as THREE from "three";
 import { getBody, getMouseBall } from "./getBodies.js";
-import RAPIER from 'https://cdn.skypack.dev/@dimforge/rapier3d-compat@0.11.2';
-import { EffectComposer } from "jsm/postprocessing/EffectComposer.js";
-import { RenderPass } from "jsm/postprocessing/RenderPass.js";
-import { UnrealBloomPass } from "jsm/postprocessing/UnrealBloomPass.js";
+import RAPIER from 'https://cdn.skypack.dev/@dimforge/rapier3d-compat';
 
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGPURenderer({ antialias: true });
+renderer.setAnimationLoop(animate);
 renderer.setSize(w, h);
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.outputColorSpace = THREE.SRGBColorSpace;
@@ -21,16 +20,6 @@ let mousePos = new THREE.Vector2();
 await RAPIER.init();
 const gravity = { x: 0.0, y: 0, z: 0.0 };
 const world = new RAPIER.World(gravity);
-
-// post-processing
-const renderScene = new RenderPass(scene, camera);
-const bloomPass = new UnrealBloomPass(new THREE.Vector2(w, h), 1.5, 0.4, 100);
-bloomPass.threshold = 0.005;
-bloomPass.strength = 2.0;
-bloomPass.radius = 0;
-const composer = new EffectComposer(renderer);
-composer.addPass(renderScene);
-composer.addPass(bloomPass);
 
 const numBodies = 100;
 const bodies = [];
@@ -49,15 +38,13 @@ scene.add(hemiLight);
 
 
 function animate() {
-  requestAnimationFrame(animate);
   world.step();
   mouseBall.update(mousePos);
   bodies.forEach(b => b.update());
-  composer.render(scene, camera);
+  renderer.render(scene, camera);
   // controls.update();
 }
 
-animate();
 
 function handleWindowResize () {
   camera.aspect = window.innerWidth / window.innerHeight;
